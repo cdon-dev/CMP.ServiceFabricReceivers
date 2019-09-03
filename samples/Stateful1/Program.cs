ae49d0c166b8e8058a3f0d925f7239caccfd4fd1
Per Ökvist <perokvist@users.noreply.github.com>
2019-09-03 14:16:51 +0200

    sample start

---
+++ samples/Stateful1/Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Serilog;
+using Serilog.Extensions.Logging;
+using ServiceFabricReceiver;
+
+namespace Stateful1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+
+
+            Log.Logger = new LoggerConfiguration()
+                .CreateLogger();
+
+            var logger = new SerilogLoggerProvider(Log.Logger, true)
+                .CreateLogger("Stateful-Sample");
+
+            try
+            {
+                ServiceRuntime.RegisterServiceAsync(
+                    "ProductReceiverType",
+                    context =>
+                        new ReceiverService(
+                         context,
+                         logger,
+                         new TelemetryClient(TelemetryConfiguration.Active),
+                         new ReceiverOptions()
+                         {
+                             ConnectionString = "",
+                             ConsumerGroup = ""
+                         },
+                         ServiceEventSource.Current.Message,
+                         (events, ct) => Task.CompletedTask,
+                            ct => Task.CompletedTask
+                   )).GetAwaiter().GetResult();
+
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(ReceiverService).Name);
+
+                // Prevents this host process from terminating so services keep running.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
